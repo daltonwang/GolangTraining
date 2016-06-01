a48acd7a46a576f89a72902041ec5f54925748eb
Zhiping Wang <zhiping.wang@gmail.com>
2016-06-01 22:22:40 +0800

    Update main.go

--- 05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
+++ 05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	//http.Get 用来获取网页
 	res, err := http.Get("http://www.mcleods.com/")
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	//ioutil.ReadAll用来解析网页
 	page, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
