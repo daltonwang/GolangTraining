fd1291474c46d5b40d5d8026dc2f111465cacc92
Zhiping Wang <zhiping.wang@gmail.com>
2016-06-05 20:00:21 +0800

    Update main.go

--- 08_pointers/04_using-pointers/01_no-pointer/01/main.go
+++ 08_pointers/04_using-pointers/01_no-pointer/01/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func zero(z int) {
-	z = 0
+	z = 0 //进入子函数后，如果需要更改这个变量，则需要引用这个变量的指针
 }
 
 func main() {
 	x := 5
-	zero(x)
+	zero(x) /
 	fmt.Println(x) // x is still 5
 }
