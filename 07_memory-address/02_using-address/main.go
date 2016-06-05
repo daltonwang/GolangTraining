fe89e8a8c477761b523987fa88e5abd8c4775fd7
Zhiping Wang <zhiping.wang@gmail.com>
2016-06-05 19:56:39 +0800

    Update main.go

--- 07_memory-address/02_using-address/main.go
+++ 07_memory-address/02_using-address/main.go
@@ -7,7 +7,7 @@ const metersToYards float64 = 1.09361
 func main() {
 	var meters float64
 	fmt.Print("Enter meters swam: ")
-	fmt.Scan(&meters)
+	fmt.Scan(&meters) //Scan 里面的参数需要是变量的地址
 	yards := meters * metersToYards
 	fmt.Println(meters, " meters is ", yards, " yards.")
 }
