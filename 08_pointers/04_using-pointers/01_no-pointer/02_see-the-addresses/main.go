82d40b8b858ac982c9f0a6453619d56872461f7b
Zhiping Wang <zhiping.wang@gmail.com>
2016-06-05 20:03:10 +0800

    Update main.go

--- 08_pointers/04_using-pointers/01_no-pointer/02_see-the-addresses/main.go
+++ 08_pointers/04_using-pointers/01_no-pointer/02_see-the-addresses/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func zero(z int) {
+func zero(z int) {//由于z是作为一个参数传进来，所以新开辟了一个空间
 	fmt.Printf("%p\n", &z) // address in func zero
 	fmt.Println(&z)        // address in func zero
 	z = 0
